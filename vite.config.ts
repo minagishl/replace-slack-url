00d829cc8f41b4655ad2b7e4727b33f8af63120c
minagishl <mvxd568.co@icloud.com>
2025-02-16 19:38:15 +0900

    chore: remove Cloudflare-related configurations

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/cloudflare" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   plugins: [
     remix({
